abb59753cd44814593229b8d7d5bf64c10ecd065
KaterinaTomashenko <tomashenko.katerina@gmail.com>
2023-03-21 22:38:52 +0300

    feat: add useState in PizzaBlock and Categories

--- src/components/Categories.jsx
+++ src/components/Categories.jsx
@@ -1,15 +1,53 @@
+import { useState } from "react";
+
 function Categories() {
+	const [activeIndexset, setActiveIndex] = useState(0);
+
+	const onClickCategories = (index) => {
+		setActiveIndex(index);
+	};
+
 	return (
-		<div class="categories">
+		<div className="categories">
 			<ul>
-				<li class="active">Все</li>
-				<li>Мясные</li>
-				<li>Вегетарианская</li>
-				<li>Гриль</li>
-				<li>Острые</li>
-				<li>Закрытые</li>
+				<li
+					onClick={() => onClickCategories(0)}
+					className={activeIndexset === 0 ? "active" : ""}
+				>
+					Все
+				</li>
+				<li
+					onClick={() => onClickCategories(1)}
+					className={activeIndexset === 1 ? "active" : ""}
+				>
+					Мясные
+				</li>
+				<li
+					onClick={() => onClickCategories(2)}
+					className={activeIndexset === 2 ? "active" : ""}
+				>
+					Вегетарианская
+				</li>
+				<li
+					onClick={() => onClickCategories(3)}
+					className={activeIndexset === 3 ? "active" : ""}
+				>
+					Гриль
+				</li>
+				<li
+					onClick={() => onClickCategories(4)}
+					className={activeIndexset === 4 ? "active" : ""}
+				>
+					Острые
+				</li>
+				<li
+					onClick={() => onClickCategories(5)}
+					className={activeIndexset === 5 ? "active" : ""}
+				>
+					Закрытые
+				</li>
 			</ul>
 		</div>
 	);
 }
-export default Categories
+export default Categories;
